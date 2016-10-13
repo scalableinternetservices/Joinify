d60c9286e9f3d11818c30544586fc897e157330b
Jonathan Easterman <jonathaneasterman@gmail.com>
2016-10-12 18:14:14 -0700

    Add username validations to user. Add tests for validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,13 +2,30 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
-	test "user should be M or F" do
-		if sex == "M" || sex == "F"
-			assert true
-		else 
-			assert false
-		end
-	end
+  test "user username should exist" do
+    user = User.new
+    assert_not user.valid?
+  end
+
+  test "user username should not be empty" do
+    user = User.new(username: "")
+    assert_not user.valid?
+  end
+
+  test "user username should not be blank" do
+    user = User.new(username: "  ")
+    assert_not user.valid?
+  end
+
+  test "user username should be at least 5 characters" do
+    user = User.new(username: "a" * 4)
+    assert_not user.valid?
+  end
+
+  test "user username should be at most 20 characters" do
+    user = User.new(username: "a" * 21)
+    assert_not user.valid?
+  end
 
 	test "user sex should be valid" do
 		user = User.new(sex: "J")
