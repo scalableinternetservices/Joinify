af7a7c776cc70cd5e7a9fff184396fcb4b3704ac
Jonathan Easterman <jonathaneasterman@gmail.com>
2016-10-17 10:05:15 -0700

    Add user model tests and validations. Update controller actions. Update user form with bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
-  validates :username, presence: true, length: { in: 5..20 }
+  before_save { self.email = email.downcase }
+  validates :username, presence: true, length: { in: 5..20, message: "Username must be between 5 and 20 characters long"}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: {case_sensitive: false}
   validates :sex, inclusion: {in: ["M", "F", nil], message: "%{value} is not M or F"}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161012231849) do
+ActiveRecord::Schema.define(version: 20161017024144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20161012231849) do
     t.string   "picture"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
 end
