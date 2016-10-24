a3646cf5f6d8dee3d9891afb0da8fcc9ae2c97d7
Jonathan Easterman <jonathaneasterman@gmail.com>
2016-10-24 16:13:09 -0700

    Add paperclip to allow users and events to have pictures. Restyle event feed page and navigation

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,9 @@ class Event < ApplicationRecord
   has_and_belongs_to_many :invitees, class_name: "User", :join_table => :invited
   has_and_belongs_to_many :attendees, class_name: "User", :join_table => :attending
   belongs_to :owner, class_name: "User"
-
+  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "metallica.jpg"
+  
+  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
   validates :title, presence: true
   validate :validate_title_length
   validates :start_date, presence: true
