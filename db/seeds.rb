eb84d213e8384de5ef7a5b543e4916178c2bb695
Jonathan Easterman <jonathaneasterman@gmail.com>
2016-11-07 14:58:40 -0800

    Modifications to db seed file to make it run faster

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,20 +7,35 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-1000.times do |i|
+# 1000.times do |i|
   
-  User.create(
-    username: "user#{i}",
-    password: "password#{i}",
-    password_confirmation: "password#{i}",
-    email: "user#{i}@example.com"
-  )
+#   User.create(
+#     username: "user#{i}",
+#     password: "password#{i}",
+#     password_confirmation: "password#{i}",
+#     email: "user#{i}@example.com"
+#   )
 
-  Event.create(
-    title: "event#{i}",
-    start_date: Time.now + i.days,
-    description: "Description for event#{i}",
-    is_public: (i % 2) == 1,
-    owner_id: i
-  )
+#   Event.create(
+#     title: "event#{i}",
+#     start_date: Time.now + i.days,
+#     description: "Description for event#{i}",
+#     is_public: (i % 2) == 1,
+#     owner_id: i
+#   )
+# end
+
+inserts = []
+1000.times do |i|
+  password = "password#{i}"
+  new_hashed_password = User.new(:password => password).encrypted_password
+  inserts.push "('user#{i}', 'user#{i}@example.com', '#{new_hashed_password}', '#{Time.now}', '#{Time.now}')"
 end
+sql = %{
+INSERT INTO users 
+(username, email, encrypted_password, created_at, updated_at) 
+VALUES #{inserts.join(", ")}
+}.squish
+
+User.connection.execute sql
+
