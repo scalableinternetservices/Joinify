d77205b0505c459b805485c920ae76d786348efb
Jonathan Easterman <jonathaneasterman@gmail.com>
2016-11-06 16:43:51 -0800

    Add db/seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+1000.times do |i|
+  
+  User.create(
+    username: "user#{i}",
+    password: "password#{i}",
+    password_confirmation: "password#{i}",
+    email: "user#{i}@example.com"
+  )
+
+  Event.create(
+    title: "event#{i}",
+    start_date: Time.now + i.days,
+    description: "Description for event#{i}",
+    is_public: (i % 2) == 1,
+    owner_id: i
+  )
+end
