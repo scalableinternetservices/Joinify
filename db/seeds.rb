4eb67e542f7f1f786b41c11da4fa75b93a897863
Jonathan Easterman <jonathaneasterman@gmail.com>
2016-11-16 16:38:44 -0800

    fix bug in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-
 # 100.times do |i|
   
 #   User.create(
@@ -40,18 +31,19 @@ user_count.times do |i|
   user_inserts.push "('user#{i}', 'user#{i}@example.com', '#{new_hashed_password}', '#{Time.now}', '#{Time.now}')"
 end
 
+puts "Finished generating users"
+
 event_count.times do |i|
   owner_id = rand(1..user_count)
   event = %{
     ('event#{i}', 
     '#{Time.now + (i+1).days}', 
     'Description for event#{i}', 
-    '#{(i % 2) == 1}' 
-    '#{owner_id}' 
+    '#{(i % 2) == 1}', 
+    '#{owner_id}', 
     '#{Time.now}', 
     '#{Time.now}')
   }.squish
-  puts event
   event_inserts.push(event)
   comments_count.times do |j|
     comment = %{
@@ -64,6 +56,8 @@ event_count.times do |i|
     comment_inserts.push(comment)
   end
 end
+
+puts "Finished generating events and comments"
 
 users_sql = %{
   INSERT INTO users 
