78fc3d192309f51fe2808a442af9f9065ec8ba73
Jonathan Easterman <jonathaneasterman@gmail.com>
2016-11-21 09:01:36 -0800

    Add code for server side caching

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,5 @@
 module EventsHelper
+  def cache_key_for_events_table
+    ("events_table/#{Event.maximum(:updated_at)}")
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-	belongs_to :event
+	belongs_to :event, touch: true
 	belongs_to :creator, class_name: "User"
 
 end
